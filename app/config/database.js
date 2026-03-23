af17cf69a24d0af35aa4486ec5d014493cf3f2bb
Ainhi2005 <ngothiainhitl@gmail.com>
2026-03-23 16:40:17 +0700

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -4,7 +4,6 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '', // Mật khẩu của bạn
     database: 'web_ban_ve',
     waitForConnections: true,
     connectionLimit: 10,
